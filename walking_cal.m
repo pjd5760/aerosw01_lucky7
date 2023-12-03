4e07d7e6f0224868fc454ef82b2917a0d07af81c
박재두 <@172.16.11.202>
2023-12-03 19:11:24 +0900

    보간법사용한 칼로리 계산법

--- walking_cal.m
+++ walking_cal.m
@@ -1,4 +1,4 @@
-function [time,dt_Burned_cal,Burned_cal] = walking_cal(T,SPD,W)
+% function [time,dt_Burned_cal,Burned_cal] = walking_cal(T,SPD,W)
 % [time,dt_Burned_cal,Burned_cal] = walking_cal(Time,speed,Body Wegiht)
 % Burned_cal(행백터): 누적된 칼로리 소모량
 % dt_Burned_cal(행벡터): 순간 칼로리 소모량
@@ -10,10 +10,10 @@ function [time,dt_Burned_cal,Burned_cal] = walking_cal(T,SPD,W)
 % W(kg) : 몸무게(스칼라)
 
 % script용도 주석 네이버 기준 80kg 60분 running 588kcal소모
-% clear;clc
-% T = 1:1:3600;
-% SPD = 12 *ones(1,length(T));
-% W = 80;
+clear;clc
+T = 1:1:3600;
+SPD = 12 *rand(1,length(T));
+W = 80;
 
 % MET : Metabolic Equivalent of Task (MET)
 MET=[];
@@ -44,7 +44,7 @@ for i = 2:length(SPD)
     burned_cal = burned_cal + dt_burned_cal;
     Burned_cal(i) = burned_cal;
 end
-end
+% end
 % total burned calories(kcal) = sum{각 활동별MET*체중*소비시간(min)/60(min)}
 % 1분당 소모한 칼로리(kcal) : 분당MET*체중*1(분)/60(분)
 % dT당 소모한 칼로리(kcal) : dT당MET*체중*{(t2-t1)sec*(1min/60sec)}/60min
